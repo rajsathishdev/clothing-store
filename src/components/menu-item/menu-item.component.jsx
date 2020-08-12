16161090f335e50b545b273c5e4fddd3afbd2bde
sathish <sathish@chennovate.com>
2020-08-12 08:22:58 +0530

    background image transition added

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -3,11 +3,12 @@ import React from 'react';
 import './menu-item.styles.scss';
 
 const MenuItem = ({title, imageUrl, size}) => (
-    <div
+    <div className={`${size} menu-item`}>
+    <div 
+      className='background-image'
       style={{
           backgroundImage: `url(${imageUrl})`
-      }}
-    className={`${size} menu-item`}>
+      }}></div>
         <div className="content">
         <div className="title">{title}</div>
             <div className="subtitle">SHOP NOW</div>
