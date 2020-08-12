fd3f7e630140fde3167aff904461afeb42bf2f8c
sathish <sathish@chennovate.com>
2020-08-12 07:52:37 +0530

    directory component and images shown

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 
 import './menu-item.styles.scss';
 
-const MenuItem = ({title}) => (
-    <div className="menu-item">
+const MenuItem = ({title, imageUrl, size}) => (
+    <div
+      style={{
+          backgroundImage: `url(${imageUrl})`
+      }}
+    className={`${size} menu-item`}>
         <div className="content">
         <div className="title">{title}</div>
             <div className="subtitle">SHOP NOW</div>
