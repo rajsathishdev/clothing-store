e9b394e2f6ac868a7e3ddf0ab2ffd9be1fdd69d7
sathish <rajsathishdev@gmail.com>
2020-08-31 09:08:27 +0530

    shop page wip

---
+++ src/components/CollectionItem/collection-item.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './collection-item.styles.scss';
+
+cons CollectionItem = ({imageUrl, price, name}) => {
+    return (
+
+    );
+}
+
+export default CollectionItem;
--- src/components/CollectionPreview/collection-preview.component.jsx
+++ src/components/CollectionPreview/collection-preview.component.jsx
@@ -2,15 +2,23 @@ import React from 'react';
 
 import './collection-preview.styles.scss';
 
-const CollectionPreview = ({id, title, items}) => {
+const CollectionPreview = ({title, items}) => {
     console.log(items);
     return (
         <div className="collection-preview">
-            {
-                items.map((item) => {
-                    return <h1 key={item.id}>{item.name }</h1>
-                })
-            }
+            <h1 className="title">
+                { title.toUpperCase() }
+            </h1>
+            <div className="preview">
+                {
+                    items
+                    .filter((item,idx)=> idx < 4)
+                    .map((item) => {
+                        return <h1 key={item.id}>{item.name }</h1>
+                    })
+                }
+            </div>
+            
         </div>
     );
 }
